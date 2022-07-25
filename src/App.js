8cfda8ef334af8443285c621e50680ce6927e88e
VladimirPariy <Vladimir.pariy@gmail.com>
2022-07-25 13:20:37 +0300

    added reducer by position

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
-import { FilterPanel } from 'components/FilterPanel';
-import { JobList } from 'components/JobList';
-import { TheHeader } from 'components/TheHeader';
+import { FilterPanel } from './components/FilterPanel';
+import { JobList } from './components/JobList';
+import { TheHeader } from './components/TheHeader';
+import {useDispatch} from "react-redux";
+import {useEffect} from "react";
+import data from './mock/data.json'
+import {addPositions} from "./store/positions/position-actions";
 
 function App() {
+    const dispatch = useDispatch()
+
+    useEffect(()=> {
+        dispatch(addPositions(data))
+    })
   return (
     <>
      <TheHeader />
